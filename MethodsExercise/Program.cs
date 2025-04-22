f6b04c4019339e994a5457ed44ec22b806b3174b
tishacapps0519 <cappstisha@gmail.com>
2025-04-22 14:01:22 -0400

    Completed exercise 2 with math operations

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,22 +2,36 @@
 {
     public class Program
     {
-        public static void MadLib()
+        
+
+        public static int Add(int numOne, int numTwo)
         {
-            Console.WriteLine("What is your name?");
-            string userName = Console.ReadLine();
-            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
-            string favColor = Console.ReadLine();
-            Console.WriteLine("What is your favorite animal?");
-            string favAnimal = Console.ReadLine();
-            Console.WriteLine("Favorite Band?");
-            string favBand = Console.ReadLine();
-            
-            Console.WriteLine($"One day, {userName} was walking through the woods while wearing a {favColor} t-shirt. {userName} saw a {favAnimal} listening to {favBand}.");
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Multiply(int numOne, int NumTwo)
+        {
+            return numOne * NumTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
         }
         static void Main(string[] args)
         {
-            MadLib();
+           Console.WriteLine(Add(numOne:2, numTwo:2));
+           //int addedNums = Add(numOne: 2, numTwo: 2);
+           //Console.WriteLine(addedNums);
+           //Console.WriteLine(Subtract(numOne:2, numTwo:2));
+           //Console.WriteLine(Multiply(numOne:2, NumTwo:2));
+           Console.WriteLine(Divide(numOne:2, numTwo:2));
+
         }
     }
 }
--- MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879bc51ef403450a0a853307a5e2bad9e738048e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f75cfd100b60569d14c4bfa4f0d57023d898919")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
