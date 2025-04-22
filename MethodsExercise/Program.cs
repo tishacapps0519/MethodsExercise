0f75cfd100b60569d14c4bfa4f0d57023d898919
tishacapps0519 <cappstisha@gmail.com>
2025-04-22 13:24:07 -0400

    Methods Exercise Story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,22 @@
 {
     public class Program
     {
+        public static void MadLib()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine("Favorite Band?");
+            string favBand = Console.ReadLine();
+            
+            Console.WriteLine($"One day, {userName} was walking through the woods while wearing a {favColor} t-shirt. {userName} saw a {favAnimal} listening to {favBand}.");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLib();
         }
     }
 }
